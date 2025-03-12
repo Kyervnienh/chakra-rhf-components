a926a3e8f4c066b3a2a92d9f541b782235c16212
Kevin López <kevin.kyervnienh@gmail.com>
2025-03-11 19:16:36 -0600

    Refactor ControlledNumberInput to import InputGroup for improved structure; update index exports to remove unused components.

--- lib/components/ControlledNumberInput.tsx
+++ lib/components/ControlledNumberInput.tsx
@@ -1,7 +1,8 @@
-import { InputGroup } from '@/components/ui/input-group';
 import { Input } from '@chakra-ui/react';
 import { Controller } from 'react-hook-form';
 import { NumericFormat } from 'react-number-format';
+
+import { InputGroup } from '@/components/ui/input-group';
 import Field from './Field';
 import type { ControlledInputNumberProps } from './index';
 
